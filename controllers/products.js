6f15b13bb02a58c3b6ec31a8570c4d5d3b9fd7ed
leomuko <kezzieleo@gmail.com>
2023-01-10 14:44:08 +0300

    Create Controller Functions for Products Api

---
+++ controllers/products.js
@@ -0,0 +1,15 @@
+
+
+const getAllProductsStatic = async (req, res) =>{
+   // throw new Error('Testing Async Error')
+    res.status(200).json({msg : 'products testing route'})
+}
+
+const getAllProducts = async (req, res) =>{
+    res.status(200).json({msg : "products route"})
+}
+
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic
+}
